c6e216851b8f39c072304729e7fc4b722c4abf1d
dmitryu95 <dmitriyu1428@gmail.com>
2021-09-29 15:54:58 +0700

    fix Lib and fix APi Requests

--- src/ApiRequest.js
+++ src/ApiRequest.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-export default ApiRequest = () => {
+const ApiRequest = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   const getMovies = async () => {
-    try {
-      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka');
+     try {
+      const response = await fetch('http://reactnative.dev/movies.json');
       const json = await response.json();
-      setData(json.ingredients);
+      setData(json.movies);
     } catch (error) {
       console.error(error);
     } finally {
@@ -28,10 +28,12 @@ export default ApiRequest = () => {
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Text>{item.strABV}</Text>
+            <Text>{item.title}, {item.releaseYear}</Text>
           )}
         />
       )}
     </View>
   );
-};
+}
+  
+export default ApiRequest
